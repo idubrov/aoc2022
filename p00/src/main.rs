5ac33f8466087591e14d631cc7765b422d8e5314
Ivan Dubrov <dubrov.ivan@gmail.com>
2022-11-30 23:07:07 -0600

    First puzzle

--- p00/src/main.rs
+++ p00/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-  println!("Hello, world!");
+  let mut all = std::fs::read_to_string("p00/src/input.txt")
+    .unwrap()
+    .split("\n\n")
+    .map(|chunk| {
+      chunk
+        .split("\n")
+        .filter(|s| !s.is_empty())
+        .map(|c| str::parse::<u32>(c).unwrap())
+        .sum()
+    })
+    .collect::<Vec<u32>>();
+  all.sort();
+  all.reverse();
+
+  eprintln!("{}", all[0]);
+  eprintln!("{}", all[0] + all[1] + all[2]);
 }
