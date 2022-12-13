26b7c6ed42850aad9195764186ebd17f06475665
Ivan Dubrov <dubrov.ivan@gmail.com>
2022-12-12 23:40:01 -0600

    better?

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,5 +1,12 @@
 use std::cmp::Ordering;
+use pest::iterators::Pair;
+use pest_derive::Parser;
+use pest::Parser;
 use aoc2022::*;
+
+#[derive(Parser)]
+#[grammar = "bin/day13/list.pest"]
+struct ListParser;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Node {
@@ -11,24 +18,7 @@ impl Ord for Node {
   fn cmp(&self, other: &Self) -> Ordering {
     match (self, other) {
       (Node::Value(l), Node::Value(r)) => l.cmp(r),
-      (Node::List(l), Node::List(r)) => {
-        for idx in 0.. {
-          if idx < l.len() && idx < r.len() {
-            if l[idx] < r[idx] {
-              return Ordering::Less;
-            } else if l[idx] > r[idx] {
-              return Ordering::Greater;
-            }
-          } else if l.len() < r.len() {
-            return Ordering::Less;
-          } else if l.len() > r.len() {
-            return Ordering::Greater;
-          } else {
-            break;
-          }
-        }
-        return Ordering::Equal;
-      },
+      (Node::List(l), Node::List(r)) => l.cmp(r),
       (lhs @ Node::Value(_), rhs @ Node::List(_)) => Node::List(vec![lhs.to_owned()]).cmp(rhs),
       (lhs @ Node::List(_), rhs @ Node::Value(_)) => lhs.cmp(&Node::List(vec![rhs.to_owned()])),
     }
@@ -41,47 +31,17 @@ impl PartialOrd for Node {
   }
 }
 
-struct Parser<'a> {
-  text: &'a [u8],
-  pos: usize,
-}
-
-impl Parser<'_> {
-  fn next(&self) -> u8 {
-    self.text[self.pos]
-  }
-  fn parse_text(text: &str) -> Node {
-    let mut parser = Parser {
-      text: text.as_bytes(),
-      pos: 0
-    };
-    parser.parse()
-  }
-  fn parse(&mut self) -> Node {
-    if self.next() == b'[' {
-      self.pos += 1;
-      let mut list = Vec::new();
-      if self.next() == b']' {
-        self.pos += 1;
-        return Node::List(list);
-      }
-      list.push(self.parse());
-      while self.next() == b',' {
-        self.pos += 1;
-        list.push(self.parse());
-      }
-      assert_eq!(self.next(), b']');
-      self.pos += 1;
-      return Node::List(list);
-    } else {
-      let len = self.text[self.pos..].iter().position(|ch| *ch < b'0' || *ch > b'9').unwrap_or(self.text[self.pos..].len());
-      let value = String::from_utf8_lossy(&self.text[self.pos..self.pos + len]).parse::<usize>().unwrap();
-      self.pos += len;
-      return Node::Value(value);
-    }
+fn parse_pair(pair: Pair<Rule>) -> Node {
+  match pair.as_rule() {
+    Rule::list => Node::List(pair.into_inner().map(parse_pair).collect::<Vec<_>>()),
+    Rule::value => Node::Value(pair.as_str().parse().unwrap()),
+    _ => unreachable!()
   }
 }
 
+fn parse_list(text: &str) -> Node {
+  parse_pair(ListParser::parse(Rule::node, text).unwrap().next().unwrap())
+}
 
 fn solve(path: &str) {
   let data = input_data(13, path);
@@ -91,9 +51,8 @@ fn solve(path: &str) {
   let mut list = Vec::new();
   for (idx, entry) in data.iter().enumerate() {
     let (first, second) = entry.split_once("\n").unwrap();
-
-    let first = Parser::parse_text(first);
-    let second = Parser::parse_text(second);
+    let first = parse_list(first);
+    let second = parse_list(second);
     if first < second {
       total += idx + 1;
     }
@@ -101,8 +60,8 @@ fn solve(path: &str) {
     list.push(second);
   }
 
-  let div1 = Parser::parse_text("[[2]]");
-  let div2 = Parser::parse_text("[[6]]");
+  let div1 = parse_list("[[2]]");
+  let div2 = parse_list("[[6]]");
 
   list.push(div1.clone());
   list.push(div2.clone());
